a3fc44c73bff56d57078f1757ba14e58c402ab1d
Guilherme Coutinho <guilhermecoutinho7777@gmail.com>
2018-09-11 23:32:39 -0300

    Adicionado animação do pássaro e configurado a queda

--- core/src/com/coutinhodeveloper/FlappyBird.java
+++ core/src/com/coutinhodeveloper/FlappyBird.java
@@ -14,13 +14,15 @@ public class FlappyBird extends ApplicationAdapter {
 
     //classe SpriteBatch é usada para criar animações com imagens
     private SpriteBatch batch;
-    private Texture passaro;
+    private Texture[] passaros;
     private Texture fundo;
 
     //atributos de configuração do jogo
-    private int movimento=0;
     private int larguraDispositivo;
     private int alturaDispositivo;
+    private float variacao = 0;
+    private float velocidadeQueda=0;
+    private float posicaoInicialVertical;
 
 
 
@@ -30,11 +32,16 @@ public class FlappyBird extends ApplicationAdapter {
 	public void create () {
 
 	    batch = new SpriteBatch();
-	    passaro = new Texture("passaro1.png");
+	    passaros = new Texture[3];
+	    passaros[0] = new Texture("passaro1.png");
+	    passaros[1] = new Texture("passaro2.png");
+	    passaros[2] = new Texture("passaro3.png");
+
         fundo = new Texture("fundo.png");
 
         larguraDispositivo = Gdx.graphics.getWidth();
         alturaDispositivo = Gdx.graphics.getHeight();
+        posicaoInicialVertical = alturaDispositivo / 2;
 
 
 
@@ -43,11 +50,18 @@ public class FlappyBird extends ApplicationAdapter {
 	@Override
 	public void render () {
 
-	    movimento ++;
+        variacao += Gdx.graphics.getDeltaTime() * 10;
+        velocidadeQueda++;
+
+
+        if (variacao >2) variacao = 0;
+        if (posicaoInicialVertical >0 )
+        posicaoInicialVertical += - velocidadeQueda;
+
 	    batch.begin();
 
 	    batch.draw(fundo,0,0,larguraDispositivo,alturaDispositivo);
-	    batch.draw(passaro, movimento, 400);
+	    batch.draw(passaros [(int) variacao], 30,posicaoInicialVertical);
 
 	    batch.end();
 
