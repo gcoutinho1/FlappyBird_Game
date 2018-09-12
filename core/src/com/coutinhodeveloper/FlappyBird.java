ea08c950a6a3baf7c318b4fae37b049536b982fe
Guilherme Coutinho <guilhermecoutinho7777@gmail.com>
2018-09-11 21:11:58 -0300

    primeira estrutura finalizada

--- core/src/com/coutinhodeveloper/FlappyBird.java
+++ core/src/com/coutinhodeveloper/FlappyBird.java
@@ -7,27 +7,37 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class FlappyBird extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+
+    //classe SpriteBatch é usada para criar animações com imagens
+
+    private SpriteBatch batch;
+    private Texture passaro;
+
+
+
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+	    batch = new SpriteBatch();
+	    passaro = new Texture("passaro1.png");
+
+
+
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
+	    batch.begin();
+
+
+        batch.draw(passaro, 0, 0);
+
+	    batch.end();
+
+
 	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+
 }
